d5bba9e812695a96e8ea32be2944ee85edf21713
Nilambar Sharma <nilambar@outlook.com>
2026-06-12 11:24:44 +0545

    Add POT

--- maintainic.php
+++ maintainic.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Maintainic
  * Plugin URI: https://github.com/ernilambar/maintainic/
- * Description: Maintenance plugin.
+ * Description: Simple maintenance plugin.
  * Version: 1.0.1
  * Requires at least: 6.6
  * Requires PHP: 7.4
@@ -11,6 +11,7 @@
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  * Text Domain: maintainic
+ * Domain Path: /languages
  *
  * @package Maintainic
  */
